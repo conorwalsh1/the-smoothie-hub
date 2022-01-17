14fac41a2fb10404bb94944d3f151dd425199a73
conorwalsh1 <conorwalsh7@gmail.com>
2022-01-17 15:15:24 +0000

    Add about link to nav bar

--- thesmoothiehub/urls.py
+++ thesmoothiehub/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
+    path('about/', include('about.urls')),
     path('', include('recipe.urls'), name='recipe_urls'),
     path('accounts/', include('allauth.urls')),
+    
 ]
