e55ee602ffb48ee45af70c87226fc6f1552da7aa
conorwalsh1 <conorwalsh7@gmail.com>
2022-01-18 16:06:23 +0000

    Make some style changes to css and add two smoothie recipes via admin

--- recipe/models.py
+++ recipe/models.py
@@ -11,6 +11,8 @@ class Post(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='recipe_posts')
     updated_on = models.DateTimeField(auto_now=True)
+    ingredients = models.TextField(blank=True)
+    method = models.TextField(blank=True)
     featured_image = CloudinaryField('image', default='placeholder')
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
