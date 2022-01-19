36450448c55dc1aca4774815bc6edf31acd89b83
conorwalsh1 <conorwalsh7@gmail.com>
2022-01-19 14:07:29 +0000

    Add CreateRecipe class to models.py of create recipe form app

--- create_recipe_form/admin.py
+++ create_recipe_form/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import CreateRecipe
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
+
+
+@admin.register(CreateRecipe)
+class CreateRecipeAdmin(SummernoteModelAdmin):
+
+    search_fields = ['ingredients', 'method']
+    summernote_fields = ('ingredients', 'method')
--- create_recipe_form/models.py
+++ create_recipe_form/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class CreateRecipe(models.Model):
+
+    title = title = models.CharField(max_length=200, unique=True, default=False)
+    ingredients = models.TextField(null=False, blank=False)
+    method = models.TextField(null=False, blank=False)
+
+    def __str__(self):
+        return self.title
